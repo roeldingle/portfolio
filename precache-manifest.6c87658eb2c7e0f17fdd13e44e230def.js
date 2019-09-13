60f9e251be5252daeea3183e0bd4b080462d1623
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-14 00:35:00 +0800

    Updates

---
+++ precache-manifest.6c87658eb2c7e0f17fdd13e44e230def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7913d3a976e342ad9a05a0385b586f57",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "00ffd970cecd2f32efc9",
+    "url": "/portfolio/static/css/2.c9ac42db.chunk.css"
+  },
+  {
+    "revision": "279123dee0ff67416b02",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "00ffd970cecd2f32efc9",
+    "url": "/portfolio/static/js/2.0d016a47.chunk.js"
+  },
+  {
+    "revision": "279123dee0ff67416b02",
+    "url": "/portfolio/static/js/main.22919679.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.057bc537216c19d42702b178060c1d13.js"
+  "/portfolio/precache-manifest.6c87658eb2c7e0f17fdd13e44e230def.js"
 );
 
 self.addEventListener('message', (event) => {
